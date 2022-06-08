f313dd68c4e01fc31e4ea791933e99dc91747258
odhs <sergiohe2@gmail.com>
2022-06-07 21:43:24 -0300

    UI: details_page responsivity
    UI improvements: details_page.dart now has responsivity

--- lib/features/movies/presentation/ui/views/pages/details_page/details_page.dart
+++ lib/features/movies/presentation/ui/views/pages/details_page/details_page.dart
@@ -2,10 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
+import '/core/utils/device_screen_info_utils.dart';
 import '/core/utils/api_utils.dart';
 import '/features/movies/domain/entities/movie_details_entity.dart';
 
-/// Shows sinopsis about a movie
+/// Shows the synopsis about a movie
 ///
 /// Body decides about build for tablet or mobile depends on width
 class DetailsPage extends StatelessWidget {
@@ -16,6 +17,9 @@ class DetailsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    // TODO maybe future localization var section: var txt_synopsis = AppLocalizations.of(context)!.synopsis;
+
     return Scaffold(
         appBar: AppBar(
           title: const Text("Movie Details"),
@@ -24,7 +28,7 @@ class DetailsPage extends StatelessWidget {
           BuildContext context,
           BoxConstraints constraints,
         ) {
-          if (constraints.maxWidth > 600) {
+          if (constraints.maxWidth >= DeviceScreenInfoUtils.tablet) {
             return _bodyTabletLayout(context);
           } else {
             return _bodyMobileLayout(context);
@@ -32,7 +36,35 @@ class DetailsPage extends StatelessWidget {
         }));
   }
 
-  /// Title with subline and edges distances, used in _fragmentRightMovieInfo
+  //////////////////////////////////////////////////////////////////////////
+  ///
+  ///     |-----------------|
+  ///     |   HERO POSTER   |
+  ///     |    Fragment     |
+  ///     |-----------------|
+  ///
+  //////////////////////////////////////////////////////////////////////////
+
+  /// FRAGMENT: Left Side of the page: Movie Poster
+  Widget _fragmentHeroPoster() {
+    return Hero(
+      tag: movie.id,
+      child: CachedNetworkImage(
+        imageUrl: API.requestImg(movie.posterPath),
+      ),
+    );
+  }
+
+  //////////////////////////////////////////////////////////////////////////
+  ///
+  ///     |----------------|
+  ///     |   MOVIE INFO   |
+  ///     |    Fragment    |
+  ///     |----------------|
+  ///
+  //////////////////////////////////////////////////////////////////////////
+
+  /// Title with subline and edges distances, used in _fragmentMovieInfo
   Widget _titleH3(BuildContext context, String str) {
     return Container(
       margin: const EdgeInsets.only(
@@ -58,7 +90,7 @@ class DetailsPage extends StatelessWidget {
     );
   }
 
-  /// Row with name and value, used in _fragmentRightMovieInfo
+  /// Row with name and value, used in _fragmentMovieInfo
   Widget _nameValueListTile(String name, String value, IconData icon) {
     return ListTile(
       leading: Icon(icon),
@@ -67,18 +99,15 @@ class DetailsPage extends StatelessWidget {
     );
   }
 
-  /// Left Side of the page
-  Widget _fragmentLeftHeroPoster() {
-    return Hero(
-      tag: movie.id,
-      child: CachedNetworkImage(
-        imageUrl: API.requestImg(movie.posterPath),
-      ),
-    );
-  }
+  /// FRAGMENT: Right/bottom side of the page: Movie Information
+  Widget _fragmentMovieInfo(BuildContext context) {
 
-  /// Right side of the page
-  Widget _fragmentRightMovieInfo(BuildContext context) {
+    // Max-with of the synopsis text: 33% of the longest side on landscape
+    var maxWidthInfo = double.infinity;
+    if (MediaQuery.of(context).orientation == Orientation.landscape) {
+      maxWidthInfo = MediaQuery.of(context).size.longestSide * .33;
+    }
+
     return Column(
       mainAxisAlignment: MainAxisAlignment.start,
       crossAxisAlignment: CrossAxisAlignment.start,
@@ -95,11 +124,15 @@ class DetailsPage extends StatelessWidget {
               .copyWith(color: Theme.of(context).disabledColor),
         ),
         _titleH3(context, AppLocalizations.of(context)!.synopsis),
-        Text(
-          movie.overview,
-          style: Theme.of(context).textTheme.bodyMedium,
-          textAlign: TextAlign.justify,
-          softWrap: true,
+        // Synopsis Text
+        ConstrainedBox(
+          constraints: BoxConstraints(maxWidth: maxWidthInfo),
+          child: Text(
+            movie.overview,
+            style: Theme.of(context).textTheme.bodyMedium,
+            textAlign: TextAlign.justify,
+            softWrap: true,
+          ),
         ),
         const SizedBox(height: 16),
         // TODO incluir na internacionalização
@@ -112,12 +145,23 @@ class DetailsPage extends StatelessWidget {
     );
   }
 
+////////////////////////////////////////////////////////////////////////////
+  ///
+  ///     | TABLET LAYOUT -----------------|
+  ///     |                                |
+  ///     | LEFT Fragment | RIGHT Fragment |
+  ///     |                                |
+  ///     |--------------------------------|
+  ///
+////////////////////////////////////////////////////////////////////////////
+
   /// Body: TABLET
   Widget _bodyTabletLayout(BuildContext context) {
     return Row(
       crossAxisAlignment: CrossAxisAlignment.center,
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
+        // LEFT Fragment
         Expanded(
           flex: 6,
           child: Align(
@@ -126,18 +170,19 @@ class DetailsPage extends StatelessWidget {
               aspectRatio: 3 / 4,
               child: Align(
                 alignment: AlignmentDirectional.topEnd,
-                child: _fragmentLeftHeroPoster(),
+                child: _fragmentHeroPoster(),
               ),
             ),
           ),
         ),
+        // RIGHT Fragment
         Expanded(
           flex: 6,
           child: Align(
             alignment: AlignmentDirectional.topStart,
             child: SingleChildScrollView(
               padding: const EdgeInsets.all(16.0),
-              child: _fragmentRightMovieInfo(context),
+              child: _fragmentMovieInfo(context),
             ),
           ),
         ),
@@ -145,28 +190,35 @@ class DetailsPage extends StatelessWidget {
     );
   }
 
-/*
-  /// Body: MOBILE2
-  Widget _bodyTabletLayout2(BuildContext context) {
-    return ListView(
-        scrollDirection: Axis.horizontal,
-        padding: const EdgeInsets.fromLTRB(16.0, 0, 16.0, 16.0),
-        children: [
-          _fragmentLeftHeroPoster(),
-          _fragmentRightMovieInfo(context),
-        ]);
-  }
-  */
+////////////////////////////////////////////////////////////////////////////
+  ///
+  ///     | HANDSET LAYOUT ---|
+  ///     |                   |
+  ///     |                   |
+  ///     |   TOP Fragment    |
+  ///     |                   |
+  ///     |                   |
+  ///     |                   |
+  ///     |                   |
+  ///     |  BOTTOM Fragment  |
+  ///     |                   |
+  ///     |                   |
+  ///     |-------------------|
+  ///
+////////////////////////////////////////////////////////////////////////////
 
   /// Body: MOBILE
   Widget _bodyMobileLayout(BuildContext context) {
     return ListView(
+        controller: ScrollController(),
         scrollDirection: Axis.vertical,
         padding: const EdgeInsets.fromLTRB(16.0, 0, 16.0, 16.0),
         children: [
-          _fragmentLeftHeroPoster(),
+          // TOP Fragment
+          _fragmentHeroPoster(),
           const SizedBox(height: 16),
-          _fragmentRightMovieInfo(context),
+          // BOTTOM Fragment
+          _fragmentMovieInfo(context),
         ]);
   }
 }
